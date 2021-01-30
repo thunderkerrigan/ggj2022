af93f823bfaa120daa6f9616aa061d3e7ffe2e2a
Stéphane Gérardot <gerardotstephane@gmail.com>
2021-01-31 00:16:08 +0100

    WIP

--- FPS Game/Assets/Scripts/PowerUpManager.cs
+++ FPS Game/Assets/Scripts/PowerUpManager.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using JetBrains.Annotations;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
+using Random = UnityEngine.Random;
 
 public class PowerUpManager : MonoBehaviour
 {
@@ -20,28 +23,46 @@ public class PowerUpManager : MonoBehaviour
         _spawnpoints = GetComponentsInChildren<Spawnpoint>().OrderBy(x => Random.value).ToList();
     }
 
+    [CanBeNull]
     private Transform GetSpawnpoint(int index)
     {
-        return _spawnpoints[index].transform;
+        if (index >= 0 && _spawnpoints.Count > index) return _spawnpoints[index].transform;
+        return null;
     }
 
     public void spawnPowerUps()
     {
-        for (var i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        for (var i = 0; i < PhotonNetwork.PlayerList.Length * 2; i++)
         {
             var spawnpoint = GetSpawnpoint(i);
-            var randomBool = (Random.value > 0.5f);
-            if (randomBool)
+            if (spawnpoint != null)
             {
-                PhotonNetwork.Instantiate(
-                    Path.Combine("PhotonPrefabs", "PowerUpMachineGun"),
-                    spawnpoint.position, spawnpoint.rotation);
-            }
-            else
-            {
-                PhotonNetwork.Instantiate(
-                    Path.Combine("PhotonPrefabs", "PowerUpSpeed"),
-                    spawnpoint.position, spawnpoint.rotation);
+                var powerUpType = (PowerUpType) Random.Range(0, Enum.GetValues(typeof(PowerUpType)).Length);
+                switch (powerUpType)
+                {
+                    case PowerUpType.MachineGun:
+                        PhotonNetwork.Instantiate(
+                            Path.Combine("PhotonPrefabs", "PowerUpMachineGun"),
+                            spawnpoint.position, spawnpoint.rotation);
+                        break;
+                    case PowerUpType.Speed:
+                        PhotonNetwork.Instantiate(
+                            Path.Combine("PhotonPrefabs", "PowerUpSpeed"),
+                            spawnpoint.position, spawnpoint.rotation);
+                        break;
+                    case PowerUpType.ReverseControl:
+                        PhotonNetwork.Instantiate(
+                            Path.Combine("PhotonPrefabs", "PowerUpReverseControl"),
+                            spawnpoint.position, spawnpoint.rotation);
+                        break;
+                    case PowerUpType.Stunt:
+                        PhotonNetwork.Instantiate(
+                            Path.Combine("PhotonPrefabs", "PowerUpStunt"),
+                            spawnpoint.position, spawnpoint.rotation);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
             }
         }
     }
