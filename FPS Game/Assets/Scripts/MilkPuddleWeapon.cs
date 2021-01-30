58c41498c4336e80987a3206b54fcdc2a9533a59
thunderkerrigan <thunderkerrigan@gmail.com>
2021-01-30 18:31:24 +0100

    dsadsad

--- FPS Game/Assets/Scripts/MilkPuddleWeapon.cs
+++ FPS Game/Assets/Scripts/MilkPuddleWeapon.cs
@@ -28,7 +28,14 @@ namespace DefaultNamespace
             if (enable)
             {
                 nextAttack = Time.time + cooldown * 1000;
-                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "MilkPool"), transform.position, Quaternion.identity);
+                var playerPosition = cam.transform.position;
+                var frontPosition = cam.transform.TransformPoint(Vector3.forward * 2);
+                var direction = (frontPosition - playerPosition).normalized;
+                var puddle = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "MilkPool"), transform.position, Quaternion.identity);
+                puddle.GetComponent<Rigidbody>().AddForceAtPosition(playerPosition, puddle.transform.position);
+                puddle.GetComponent<Rigidbody>().velocity = direction * 10;
+                var randomTorque = Random.insideUnitSphere;
+                puddle.GetComponent<Rigidbody>().AddTorque(randomTorque*75);
                 StartCoroutine(OnCooldown());
                 return cooldown;
             }
