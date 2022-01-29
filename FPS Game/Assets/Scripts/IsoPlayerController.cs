214a9127e33e182f51a953d04b8c7a573709765c
thunderkerrigan <thunderkerrigan@gmail.com>
2022-01-29 21:55:48 +0100

    gg

--- FPS Game/Assets/Scripts/IsoPlayerController.cs
+++ FPS Game/Assets/Scripts/IsoPlayerController.cs
@@ -106,11 +106,6 @@ public class IsoPlayerController : MonoBehaviour
    private void triggerAttack()
    {
       Vector3 directionVector3 = weapon.transform.position + new Vector3(attackVal.x, 0, attackVal.y);
-      
-      Debug.Log("directionVector3: " + directionVector3);
-      Debug.Log("Attack val : " + attackVal);
-      Debug.Log("transform val : " + weapon.transform.position);
-
       weapon.transform.rotation = Quaternion.LookRotation(new Vector3(attackVal.x, 0, attackVal.y), Vector3.up);
    }
    
