311d011b791180fdb00c07bc1c26f22a9435194d
Thomas Malval <thomas.malval@smartmobilefactory.com>
2022-01-29 11:25:07 +0100

    Enemy change direction

--- FPS Game/Assets/Scripts/Enemy/EnemyMove.cs
+++ FPS Game/Assets/Scripts/Enemy/EnemyMove.cs
@@ -10,7 +10,8 @@ using UnityEngine.AI;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 
-public class EnemyMove: MonoBehaviourPunCallbacks {
+public class EnemyMove : MonoBehaviourPunCallbacks
+{
 
     [Tooltip("Aribitrary value for enemy speed")]
     [SerializeField] private int speed = 100;
@@ -20,33 +21,68 @@ public class EnemyMove: MonoBehaviourPunCallbacks {
 
     private NavMeshAgent navMeshAgent;
 
+    private PhotonView PView;
 
-    private void Start() {
+    private void Start()
+    {
         navMeshAgent = this.GetComponent<NavMeshAgent>();
 
-        if (navMeshAgent == null) {
-            Debug.LogError("No navMeshAgent for " + gameObject.name);
-        } else {
-            Garden[] gardens = (Garden[]) GameObject.FindObjectsOfType(typeof(Garden));
-            Garden closestGarden = null;
-            float minDistance = 10000;
-            foreach (Garden garden in gardens) {
-                // Don't go to dead gardens
-                if (garden.isAlive() == false) { continue; }
+        if (this.PView == null)
+        {
+            this.PView = GetComponent<PhotonView>();
 
-                float distance = Vector3.Distance(garden.transform.position, this.transform.position);
-                if (closestGarden == null) {
-                    closestGarden = garden;
-                    minDistance = distance;
-                } else if (distance < minDistance) {
-                    closestGarden = garden;
-                    minDistance = distance;
-                }
+            if (this.PView == null) {
+                Debug.LogError("No PhotonView for " + gameObject.name);
             }
-            
-            this.destinationGarden = closestGarden;
-            this.destinationPoint = closestGarden.transform;
-            navMeshAgent.SetDestination(closestGarden.transform.position);
+
+            // if (!this.PView.IsMine)
+            // {
+            //     return;
+            // }
         }
+
+        if (navMeshAgent == null)
+        {
+            Debug.LogError("No navMeshAgent for " + gameObject.name);
+        }
+        else
+        {
+            FindClosestDestination();
+        }
+    }
+
+    private void FindClosestDestination()
+    {
+        Garden[] gardens = (Garden[])GameObject.FindObjectsOfType(typeof(Garden));
+        Garden closestGarden = null;
+        float minDistance = 10000;
+        foreach (Garden garden in gardens)
+        {
+            // Don't go to dead gardens
+            if (garden.isAlive() == false) { continue; }
+
+            float distance = Vector3.Distance(garden.transform.position, this.transform.position);
+            if (closestGarden == null)
+            {
+                closestGarden = garden;
+                minDistance = distance;
+            }
+            else if (distance < minDistance)
+            {
+                closestGarden = garden;
+                minDistance = distance;
+            }
+        }
+
+        this.destinationGarden = closestGarden;
+        this.destinationPoint = closestGarden.transform;
+        navMeshAgent.SetDestination(closestGarden.transform.position);
+    }
+
+    private void Update()
+    {
+        // if (PView.IsMine == false) { return; }
+
+        FindClosestDestination();
     }
 }
