8c7731d1933590bf4c35b1320f4d97e70853110a
Thomas Malval <thomas.malval@smartmobilefactory.com>
2022-01-29 15:47:32 +0100

    WIP

--- FPS Game/Assets/Scripts/Phase/PhaseManager.cs
+++ FPS Game/Assets/Scripts/Phase/PhaseManager.cs
@@ -7,7 +7,30 @@ using UnityEngine;
 
 public class PhaseManager : MonoBehaviourPunCallbacks {
 
-    [SerializeField] Phase currentPhase;
+    private EnemySpawnManager enemySpawnManager;
+
+    [SerializeField] private Phase currentPhase;
     
+    private void Start() {
+        enemySpawnManager = GameObject.FindObjectOfType<EnemySpawnManager>();
+        if (enemySpawnManager == null) {
+            Debug.LogError("No enemySpawnManager");
+        }
+
+
+    }
+    
+
+    IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(this.attackCooldown);
+
+        if (this.isAlive == false) { StopCoroutine(Attack()); yield break; }
+        if (this.mode != EnemyMode.Attack) { StopCoroutine(Attack()); yield break; }
+
+        this.Attack(garden: this.destinationGarden);
+
+        StartCoroutine(Attack());
+    }
     
 }
