bec83b257542a2eb62c3cb09f3c7cbf2e11af063
Thomas Malval <thomas.malval@smartmobilefactory.com>
2022-01-30 16:47:26 +0100

    Update default string on ScoreView

--- FPS Game/Assets/Scripts/ScoreSingleton.cs
+++ FPS Game/Assets/Scripts/ScoreSingleton.cs
@@ -23,7 +23,7 @@ public class ScoreSingleton : MonoBehaviour
 
     private Dictionary<string, List<string>> playerObjects = new Dictionary<string, List<string>>();
 
-    public string scoreText = "Victory";
+    public string scoreText = "All garden were eaten";
 
     public bool playerHaveFinish(string playerName)
     {
